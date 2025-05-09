23610f829a752fa4d3d86d07a4b1176d86df0f8a
shunyun <ziscloud@gmail.com>
2025-05-09 21:40:31 +0800

    remove he baseUrl config, it makes proxy not work

--- src/app.tsx
+++ src/app.tsx
@@ -131,6 +131,5 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
  * @doc https://umijs.org/docs/max/request#配置
  */
 export const request: RequestConfig = {
-  baseURL: 'https://proapi.azurewebsites.net',
   ...errorConfig,
 };
